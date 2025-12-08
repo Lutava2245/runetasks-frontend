1e9d8f29977bf2b1fcc245bc5b2000bbfabae9dc
Luan T. Felix <lutavares.bros@gmail.com>
2025-12-08 18:15:10 -0300

    :sparkles: adiciona interfaces com responses e requests de usuário e autenticação

---
+++ src/types/user.ts
@@ -0,0 +1,22 @@
+export interface UserResponse {
+  id: number
+  name: string
+  nickname: string,
+  email: string
+  currentAvatarIcon: string
+  currentAvatarName: string
+  level: number
+  xpToNextLevel: number
+  levelPercentage: number
+  progressXP: number
+  totalXP: number
+  totalCoins: number
+  createdAt: Date
+};
+
+export interface UserCreateRequest {
+  name: string
+  nickname: string
+  password: string
+  email: string
+}
