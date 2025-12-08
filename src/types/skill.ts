a0974e87e879cbb780b62553d38a4bc9d41a37bf
Luan T. Felix <lutavares.bros@gmail.com>
2025-12-08 18:52:14 -0300

    :sparkles: adiciona interface com response de habilidade

---
+++ src/types/skill.ts
@@ -0,0 +1,11 @@
+export interface SkillResponse {
+  id: number
+  name: string
+  icon: string
+  level: number
+  xpToNextLevel: number
+  levelPercentage: number
+  progressXP: number
+  totalXP: number
+  totalTasks: number
+}
