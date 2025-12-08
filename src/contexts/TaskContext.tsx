b7429dfc821068f80a8a2945767138de3e342451
Luan T. Felix <lutavares.bros@gmail.com>
2025-12-08 18:51:27 -0300

    :sparkles: adiciona TaskContext para lidar com tarefas

---
+++ src/contexts/TaskContext.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { createContext, useContext, useState, useEffect } from "react";
+import { getAllTasksByUser } from "@/src/services/taskService";
+import type { TaskResponse } from "@/src/types/task";
+import { useAuth } from "./AuthContext";
+
+interface TaskContextType {
+  tasks: TaskResponse[];
+  refreshTasks: () => Promise<void>;
+}
+
+const TaskContext = createContext<TaskContextType | null>(null);
+
+export const TaskProvider = ({ children }: { children: React.ReactNode }) => {
+  const [tasks, setTasks] = useState<TaskResponse[]>([]);
+  const { user } = useAuth();
+
+  useEffect(() => {
+    refreshTasks();
+  }, []);
+
+  const refreshTasks = async () => {
+    if (user) {
+      try {
+        const response = await getAllTasksByUser(user.id);
+        setTasks(response.data);
+      } catch (error: any) {
+        console.error(error?.response?.data);
+      }
+    }
+  };
+
+  return (
+    <TaskContext.Provider value={{ tasks, refreshTasks }}>
+      {children}
+    </TaskContext.Provider>
+  );
+};
+
+export const useTasks = () => {
+  const ctx = useContext(TaskContext);
+  if (!ctx) throw new Error("useTasks precisa estar dentro de <TaskProvider>");
+  return ctx;
+};
