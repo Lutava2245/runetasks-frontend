42aa3b3eb91513252f00069e3299b88cbf26129b
Luan T. Felix <lutavares.bros@gmail.com>
2025-12-08 19:09:51 -0300

    :sparkles: implementa componente Progress para barras de progresso

---
+++ src/components/Progress.tsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import clsx from "clsx";
+
+interface ProgressProps extends React.HTMLAttributes<HTMLDivElement> {
+  value: number;
+}
+
+const Progress = React.forwardRef<HTMLDivElement, ProgressProps>(
+  ({ className, value, ...props }, ref) => {
+    const progressValue = Math.min(100, Math.max(0, value || 0));
+
+    return (
+      <div
+        ref={ref}
+        className={clsx(
+          "relative h-4 w-full overflow-hidden rounded-full bg-gray-300",
+          className
+        )}
+        role="progressbar"
+        aria-valuenow={progressValue}
+        aria-valuemin={0}
+        aria-valuemax={100}
+        {...props}
+      >
+        <div
+          className="h-full bg-blue-600 transition-all duration-500 ease-out"
+          style={{ width: `${progressValue}%` }}
+        />
+      </div>
+    );
+  }
+);
+Progress.displayName = "Progress";
+
+export default Progress;
