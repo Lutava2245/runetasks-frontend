14e89948280d8059893386d3f7d4fee260749654
Luan T. Felix <lutavares.bros@gmail.com>
2026-01-31 17:44:10 -0300

    :lipstick: configura Toaster com cores e ícones personalizados

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/src/contexts/AuthContext";
+import { Toaster } from "sonner";
+import { AlertCircle, Ban, CheckCircle2, Info } from "lucide-react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +33,25 @@ export default function RootLayout({
         <AuthProvider>
           {children}
         </AuthProvider>
+        <Toaster
+          toastOptions={{
+            classNames: {
+              toast: '!bg-(--card) !border-2 !rounded-full !shadow-xl !text-sm',
+              title: '!text-(--foreground) !font-bold',
+              description: '!text-(--foreground) !opacity-80',
+              success: '!border-(--secondary)',
+              error: '!border-(--error) !text-(--error)',
+              info: '!border-(--primary)',
+              warning: '!border-(--warning)'
+            }
+          }}
+          icons={{
+            success: <CheckCircle2 size={20} color="var(--secondary)" />,
+            error: <AlertCircle size={20} color="var(--error)" />,
+            info: <Info size={20} color="var(--primary)" />,
+            warning: <Ban size={20} color="var(--warning)" />,
+          }}
+        />
       </body>
     </html>
   );
