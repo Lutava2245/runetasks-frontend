f9808a81e7474f6ece1288b44069567e19dfccd8
Luan T. Felix <lutavares.bros@gmail.com>
2026-02-19 22:51:23 -0300

    :lipstick: configura novas fontes no projeto - substitui icone de template

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Exo_2 } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/src/contexts/AuthContext";
 import { Toaster } from "sonner";
 import { AlertCircle, Ban, CheckCircle2, Info } from "lucide-react";
 import ReactQueryProvider from "@/src/providers/ReactQueryProvider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const geistSans = Exo_2({
+  weight: ["300", "400", "500", "600"],
+  variable: "--font-body",
   subsets: ["latin"],
 });
 
@@ -27,9 +23,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} antialiased`}
       >
         <ReactQueryProvider>
           <AuthProvider>
